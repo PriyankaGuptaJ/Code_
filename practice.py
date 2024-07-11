8454c9da5274ae76cdeed6af242fce4a8267532e
Priyanka Gupta <168647810+PriyankaGuptaJ@users.noreply.github.com>
2024-07-11 17:15:41 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,28 @@
+
+food_choice = input("enter fav snack (press e to exit) ")
+
+if food_choice!= "e":
+    print(f"your choice is {food_choice},great!")
+    food_choice = input("enter another fav snack (press e to exit) ")
+    if food_choice!= "e":
+        print("great, now I know")
+    else:
+        quit()
+else:
+    quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
